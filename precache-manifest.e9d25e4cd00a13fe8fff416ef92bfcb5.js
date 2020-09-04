c9a40985aaf23e30849244a56ad958649ab92587
epg3 <epgarces3@gmail.com>
2020-09-04 22:36:35 +0200

    Updates

---
+++ precache-manifest.e9d25e4cd00a13fe8fff416ef92bfcb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b4ee30a2301ce823ecf9cdbf636b3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d863f3e016754be9ebcf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75fb3525307be9eb384c",
+    "url": "/static/js/2.1c4dbc9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1c4dbc9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d863f3e016754be9ebcf",
+    "url": "/static/js/main.dd1becd2.chunk.js"
+  },
+  {
+    "revision": "fabd8373f8cba2eda270",
+    "url": "/static/js/runtime-main.047253d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
